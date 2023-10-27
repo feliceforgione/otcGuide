a10e0c3a8c05de794d6704aed5bd7139026e09d1
Felice Forgione <forgione67@gmail.com>
2023-10-27 19:04:02 -0400

    show confirmation dialog on delete button click

--- app/admin/disease-classes/[id]/DeleteDiseaseClassButton.tsx
+++ app/admin/disease-classes/[id]/DeleteDiseaseClassButton.tsx
@@ -1,12 +1,36 @@
-import { Button } from "@radix-ui/themes";
-import React from "react";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 function DeleteDiseaseClassButton({
   diseaseClassId,
 }: {
   diseaseClassId: number;
 }) {
-  return <Button color="red">Delete Disease Class</Button>;
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Disease Class</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this?
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button color="gray" variant="soft">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red">Delete</Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
+  );
 }
 
 export default DeleteDiseaseClassButton;
