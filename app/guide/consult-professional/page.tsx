3263f278a8f1977220858333ebcf84e987badc64
Felice Forgione <forgione67@gmail.com>
2023-11-16 19:07:13 -0500

    update consult professional component

--- app/guide/consult-professional/page.tsx
+++ app/guide/consult-professional/page.tsx
@@ -1,12 +1,25 @@
+"use client";
 import { Heading, Box, Text, Flex } from "@radix-ui/themes";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
+const ERRORS: Record<number, string> = {
+  1: "Selected one of the exclusion questions",
+  2: "No starting algorithm found",
+  3: "Filter question exclusion",
+  4: "No treatment plan id",
+};
+
 function ConsultMedicalProfessional() {
+  const searchParams = useSearchParams();
+  const errorCode = searchParams.get("error");
+
   return (
     <>
       <Heading className="pageHeading">
-        Referral : Please Consult a Medical Professional{" "}
+        Referral : Please Consult a Medical Professional
       </Heading>
+
       <Box className="flex justify-center items-center h-[70vh]">
         <Box className="rounded-full w-[30rem] h-[30rem] border-8 border-red-800 flex justify-center items-center p-8">
           <Text size={"7"} className="text-center text-red-900 ">
@@ -15,6 +28,9 @@ function ConsultMedicalProfessional() {
           </Text>
         </Box>
       </Box>
+      <Text size={"1"} color="red">
+        {errorCode && ERRORS[parseInt(errorCode)]}
+      </Text>
     </>
   );
 }
