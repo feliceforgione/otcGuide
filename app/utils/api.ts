95d8706f1c4f0fd619ff946584a547fd122c5dca
Felice Forgione <forgione67@gmail.com>
2023-11-17 21:13:28 -0500

    update api functions

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -20,3 +20,42 @@ export const getAlgorithmQuestion = async (id: number) => {
   const response = await otcGuideAPI.get(`/algorithm/${id}`);
   return response.data;
 };
+
+export const getStartingUPCs = async (id: number) => {
+  const response = await otcGuideAPI.get(`/upc/plan/${id}`);
+  return response.data;
+};
+
+export const getContraindications = async (upcs: string) => {
+  const response = await otcGuideAPI.get(
+    `/upc/medical-conditions/?upcs=${upcs}`
+  );
+  return response.data;
+};
+
+export const getFilterQuestions = async (upcs: string) => {
+  const response = await otcGuideAPI.get(`/upc/filter-questions/?upcs=${upcs}`);
+  return response.data;
+};
+
+export const getSymptoms = async (id: number) => {
+  const response = await otcGuideAPI.get(`/algorithm/symptoms/${id}`);
+  return response.data;
+};
+
+export const getSymptomsCombo = async (id: number, combo: string) => {
+  const response = await otcGuideAPI.get(
+    `/algorithm/symptoms-combo/${id}/?combo=${combo}`
+  );
+  return response.data;
+};
+
+export const getAllergies = async (upcs: string) => {
+  const response = await otcGuideAPI.get(`/upc/allergies/?upcs=${upcs}`);
+  return response.data;
+};
+
+export const getTreatmentPlan = async (id: number, upcs: string) => {
+  const response = await otcGuideAPI.get(`/plan/${id}/?upcs=${upcs}`);
+  return response.data;
+};
